4c43ea5209acd5e6a7ef1efa30b85d6f1366788b
Sam Jespersen <heyheysamj@gmail.com>
2020-01-06 15:50:00 -0800

    new build

--- src/components/Portfolio-maze-api.js
+++ src/components/Portfolio-maze-api.js
@@ -22,7 +22,7 @@ const PortfolioMazeApi = () => {
         </div>
       </div>
       <div className={styles.Portfolio}>
-        <div style={{ height: '150px', width: '100%' }}><img src='src/assets/maze-api.png' style={{ height: '100%', width: '100%' }} /></div>
+        <div style={{ height: '150px', width: '100%' }}><img src='./maze-api.png' style={{ height: '100%', width: '100%' }} /></div>
       </div>
       <div style={{ margin: 'auto', width: '70%', color: '#E5ECE9', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
         <p style={{ color: '#E5ECE9', marginTop: '0px' }}><a href="https://maze-api.herokuapp.com">Deployed Website</a></p>
