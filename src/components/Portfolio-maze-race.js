28aa347ed45069c055b6d034312d78c94a061857
Sam Jespersen <heyheysamj@gmail.com>
2020-01-06 16:01:36 -0800

    new build using github to host images

--- src/components/Portfolio-maze-race.js
+++ src/components/Portfolio-maze-race.js
@@ -22,7 +22,7 @@ const PortfolioMazeRace = () => {
         </div>
       </div>
       <div className={styles.Portfolio}>
-        <div style={{ height: '150px', width: '100%' }}><img src='/maze-race.png' style={{ height: '100%', width: '100%' }} /></div>
+        <div style={{ height: '150px', width: '100%' }}><img src='https://raw.githubusercontent.com/samjespersen/samjespersen.com/dev/src/assets/maze-race.png' style={{ height: '100%', width: '100%' }} /></div>
       </div>
       <div style={{ margin: 'auto', width: '70%', color: '#E5ECE9', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
         <p style={{ color: '#E5ECE9', marginTop: '0px' }}><a href="https://maze-race.netlify.com">Deployed Website</a></p>
